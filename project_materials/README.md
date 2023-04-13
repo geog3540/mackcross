7a09a01f7578c599214ea76c73394a7f936ad117
mackcross <mackenzie-cross@uiowa.edu>
2023-04-12 21:04:06 -0500

    IMPORTANT

---
+++ project_materials/README.md
@@ -0,0 +1,5 @@
+# INTRODUCTION
+This project explores the macroevolutionary spatial trends accross 
+primates. Given the extent of this project, the first analysis looks only 
+at Miocene apes as an exploratory analysis. The file, initial_raw_data, 
+contains information for all primates and will be used in later analyses. 
---
+++ project_materials/pleisoadapiform/data_clean_coordinate_only.R
@@ -0,0 +1,40 @@
+rm(list = ls())
+library(readr)
+df <- read_csv("~/Documents/research_projects/primate_map/initial_raw_data/primates_coordinates_a.csv")
+
+# Filtered out entries without preexisting date and GIS coordinates 
+df <- subset(df, max_ma > 0)
+
+# Adjusted data types
+df$state <- factor(df$state)
+df$lng <- factor(df$lng)
+df$lat <- factor(df$lat)
+
+names(df)
+# Deleted unnecessary columns 
+df <- df[, c("accepted_name", "max_ma", "min_ma", "lng", "lat", "state", "county")]
+
+# Made species name R friendly 
+df$accepted_name <- sub(" ", "_", df$accepted_name)
+
+# Removed redundancy in coordinates
+library(dplyr)
+df2 <- distinct(df)
+
+df3 <- subset(df2, state != "Fayum" | is.na(state))
+
+df3$genus <- df3$accepted_name
+names(df3)
+df$genus <- gsub("_", " ", df$genus, fixed=TRUE)
+
+getwd()
+# Set new working directly in console 
+# write_csv(df3, "cleaned_primate_data.csv")
+
+
+
+
+
+
+
+
