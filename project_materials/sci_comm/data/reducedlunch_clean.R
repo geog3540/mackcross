0c03c44cf6e5d348e3fef47f506c4d015692bb24
mackcross <mackenzie-cross@uiowa.edu>
2023-05-01 07:46:00 -0500

    refining maps

---
+++ project_materials/sci_comm/data/reducedlunch_clean.R
@@ -0,0 +1,39 @@
+rm(list=ls())
+
+library(readr)
+library(dplyr)
+library(rgdal)
+
+# Fill in missing csv values
+df <- read_csv("Documents/research/primate_map/project_materials/sci_comm/data/reducedmeals.csv")
+
+df$county_name <- gsub("'", "", df$county_name)
+df$county_name <- gsub("OBrien", "Obrien", df$county_name)
+
+library(dplyr)
+df_filled <- df %>%
+  group_by(county) %>%
+  mutate(county_name = ifelse(is.na(county_name), county_name[!is.na(county_name)][1], county_name))
+
+
+my_shapefile <- readOGR(dsn = "Documents/research/primate_map/project_materials/sci_comm/shapefiles/original_Iowa/IowaCounties.shp")
+
+
+merged_df <- merge(df_filled, my_shapefile@data[c("CountyName", "OBJECTID")], 
+                   by.x = "county_name", by.y = "CountyName", all.x = TRUE)
+
+merged_df$total_reduced_free <- as.numeric(merged_df$total_reduced_free)
+merged_df$k12_enrollment <- as.numeric(merged_df$k12_enrollment)
+
+grouped <- merged_df %>%
+  group_by(OBJECTID, county_name, year) %>%
+  summarize(import_ratio = sum(k12_enrollment, na.rm = TRUE) / sum(total_reduced_free, na.rm = TRUE),
+            .groups = "drop")
+head(grouped)
+
+write.csv(grouped, "Documents/research/primate_map/project_materials/sci_comm/data/cleaned_grouped_data.csv", row.names = FALSE)
+# write.csv(grouped, "grouped_data.csv", row.names = FALSE)
+
+
+
+
